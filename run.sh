64f8b049072a041d70a652aab8b32a56704808c3
ColdNightHour <jdixo001@ucr.edu>
2017-01-21 16:38:03 -0800

    Script should now work

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!
 cd ./TweetCollector
 
-if[ "$1" = "compile" ];
-then
+if [ "$1" = "compile" ]; then
   mvn package
-else if[ "$1" = "exec" ];
-then
+elif [ "$1" = "exec" ]; then
   java -cp target/TweetCollector-1.0-SNAPSHOT.jar HashtagAnalyzer.App
-else if[ "$1" = "both" ];
-then
+elif [ "$1" = "both" ]; then
   mvn package && java -cp target/TweetCollector-1.0-SNAPSHOT.jar HashtagAnalyzer.App
 else
   echo "Wrong command"
