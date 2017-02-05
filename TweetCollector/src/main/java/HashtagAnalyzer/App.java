28f419cb5a53da82cf93a5d6d15bafcf8da63ce1
ColdNightHour <jdixo001@ucr.edu>
2017-02-04 19:52:38 -0800

    Updated the stuff

--- TweetCollector/src/main/java/HashtagAnalyzer/App.java
+++ TweetCollector/src/main/java/HashtagAnalyzer/App.java
@@ -23,8 +23,8 @@ public class App{
   public static String location = "NULL";
   public static Boolean run = true;
   public static StringBuffer buffer = new StringBuffer("");
-  public static int fileCnt = 311;
-  public static int filePassCnt = 5;
+  public static int fileCnt = 403;
+  public static int filePassCnt = 3;
   public static long overallBytes = 0;
   public static PrintWriter writer;
     public static void main( String[] args )  {
